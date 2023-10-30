580daf603ac38998d2d31bc665242f8748650ba0
akhdanrgya <akhdan.anargya@gmail.com>
2023-10-30 20:21:34 +0700

    update email

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,29 +1,28 @@
-// import { EmailTemplate } from '../../../components/EmailTemplate';
-import { NextResponse } from "next/server";
-import { Resend } from "resend";
+// import { NextResponse } from "next/server";
+// import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
+// const resend = new Resend(process.env.RESEND_API_KEY);
+// const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST(req, res) {
-  const { email, subject, message } = await req.json();
-  console.log(email, subject, message);
-  try {
-    const data = await resend.emails.send({
-      from: 'Akhdan <akhdan.anargya@gmail.com>',
-      to: ['akhdan.anargya@gmail.com'],
-      subject: subject,
-      react: (
-        <>
-          <h1>{subject}</h1>
-          <p>Thank you for contacting us!</p>
-          <p>New message submitted:</p>
-          <p>{message}</p>
-        </>
-      ),
-    });
-    return NextResponse.json(data);
-  } catch (error) {
-    return NextResponse.json({ error });
-  }
-}
+// export async function POST(req, res) {
+//   const { email, subject, message } = await req.json();
+//   console.log(email, subject, message);
+//   try {
+//     const data = await resend.emails.send({
+//       from: 'akhdanrgya@gmail.com',
+//       to: ['akhdan.anargya@gmail.com'],
+//       subject: subject,
+//       react: (
+//         <>
+//           <h1>{subject}</h1>
+//           <p>Thank you for contacting us!</p>
+//           <p>New message submitted:</p>
+//           <p>{message}</p>
+//         </>
+//       ),
+//     });
+//     return NextResponse.json(data);
+//   } catch (error) {
+//     return NextResponse.json({ error });
+//   }
+// }
