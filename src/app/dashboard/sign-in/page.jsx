7b2ddbf73c3273d297773a05762ec54757feb654
akhdanrgya <akhdan.anargya@gmail.com>
2023-11-21 16:39:46 +0700

    update signin

--- src/app/dashboard/sign-in/page.jsx
+++ src/app/dashboard/sign-in/page.jsx
@@ -11,7 +11,9 @@ const Signin = () => {
   const router = useRouter()
 
 
-  const submitHandler = () => {
+  const submitHandler = (e) => {
+    e.preventDefault()
+
     if (email !== "test@gmail.com"){
         setMsg("Email tidak valid")
     }else if (password !== "12345"){
@@ -19,13 +21,14 @@ const Signin = () => {
     }else if (email !== "test@gmail.com" || password !== "12345"){
         setMsg("Email dan Password tidak valid")
     }else{
-        
+        setIsAuth(true)
+        router.push('/dashboard/admin')
     }
   }
 
   return (
     <>
-      <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+      <div className="min-h-screen bg-dark py-6 flex flex-col justify-center sm:py-12">
         <div className="relative py-3 sm:max-w-xl sm:mx-auto">
           <div className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
           <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
