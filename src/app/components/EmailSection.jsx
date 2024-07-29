1dd6b5661bcbad5663a92956ce91d41c5dc83804
akhdanrgya <akhdan.anargya@gmail.com>
2024-07-29 17:41:20 +0700

    kotak final

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -56,12 +56,12 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="https://github.com/akhdanrgya">
+          {/* <Link href="https://github.com/akhdanrgya">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
           <Link href="https://www.linkedin.com/in/akhdan-anargya-b67100270/">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
-          </Link>
+          </Link> */}
         </div>
       </div>
       <div>
